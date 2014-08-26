6786ffd2e3ba5b4ffef9e419a0c2935efc1a5472
Hadrien Lanneau <hadrien@Trololololol.local>
2014-08-26 10:07:36 +0200

    Wiredep

---
+++ app/public/scripts/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('selftym', [])
+.run(function () {
+  console.log('kikoo');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,33 @@
+'use strict';
+
 var gulp = require('gulp'),
+  $ = require('gulp-load-plugins')(),
   expressService = require('gulp-express-service');
+
+gulp.task('default', function () {
+  gulp.start('build');
+});
+
+gulp.task('lint', function () {
+  return gulp.src(['app/public/scripts/**/*.js', 'test/**/*.js', 'app/server/**/*.js'])
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail'))
+    .on('error', function () {
+      // Make sure failed tests cause gulp to exit non-zero
+      process.exit(1);
+    });
+});
+
+gulp.task('wiredep', function () {
+  var wiredep = require('wiredep').stream;
+  return gulp.src(['app/public/index.html'])
+    .pipe(wiredep({
+      directory: 'app/public/bower_components',
+      exclude: [ /bootstrap/, /jquery/ ]
+    }))
+    .pipe(gulp.dest('app/public'));
+});
 
 gulp.task('server', function () {
   return gulp.src('app/index.js')
@@ -8,16 +36,4 @@ gulp.task('server', function () {
 
 gulp.task('watch', ['server'], function () {
   gulp.watch(['app/**/*.js'], ['server']);
-});
-
-gulp.task('wiredep', function () {
-  var wiredep = require('wiredep').stream;
-  return gulp.src('public/index.html')
-    .pipe(wiredep({
-      directory: 'app/bower_components',
-      exclude: [ 'bower_components/angular/angular.js', 'bower_components/angular-scenario/angular-scenario' ],
-      dependencies: true,
-      devDependencies: false
-    }))
-    .pipe(gulp.dest('app'));
 });
