98e7a435373d65be5fe7640956448a7c9f07c254
Hadrien Lanneau <hadrien@Trololololol.local>
2014-09-11 09:34:41 +0200

    Join channel quick design

---
+++ app/public/scripts/components/photobooth/auto-selfy-controller.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('selftym')
+.controller('AutoSelfyCtrl', function ($scope, $timeout) {
+  $scope.interval = 1;
+  $scope.pause = false;
+  $scope.onStream = function (video) {
+    takeSelfy(video);
+  };
+  $scope.error = function (error) {
+    $scope.onError({error: error});
+  };
+  $scope.togglePause = function () {
+    $scope.pause = !$scope.pause;
+  };
+
+  function takeSelfy(video) {
+    if (!$scope.pause) {
+      $scope.onSelfy({
+        selfy: getVideoData(video, 0, 0, video.width, video.height)
+      });
+    }
+
+    $timeout(function () {
+      takeSelfy(video);
+    }, $scope.interval * 1000);
+  }
+
+  function getVideoData(video) {
+    var hiddenCanvas = angular.element('<canvas></canvas>')[0];
+    hiddenCanvas.width = video.width;
+    hiddenCanvas.height = video.height;
+    var ctx = hiddenCanvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, video.width, video.height);
+
+    return hiddenCanvas.toDataURL();
+  }
+});
--- app/public/scripts/components/photobooth/auto-selfy-directive.js
+++ app/public/scripts/components/photobooth/auto-selfy-directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('selftym')
-.directive('autoSelfy', function ($timeout) {
+.directive('autoSelfy', function () {
   return {
     restrict: 'E',
     templateUrl: 'scripts/components/photobooth/auto-selfy.html',
@@ -9,40 +9,6 @@ angular.module('selftym')
       onError: '&',
       onSelfy: '&'
     },
-    controller: function ($scope) {
-      $scope.interval = 1;
-      $scope.pause = false;
-      $scope.onStream = function (video) {
-        takeSelfy(video);
-      };
-      $scope.error = function (error) {
-        $scope.onError({error: error});
-      };
-      $scope.togglePause = function () {
-        $scope.pause = !$scope.pause;
-      };
-
-      function takeSelfy(video) {
-        if (!$scope.pause) {
-          $scope.onSelfy({
-            selfy: getVideoData(video, 0, 0, video.width, video.height)
-          });
-        }
-
-        $timeout(function () {
-          takeSelfy(video);
-        }, $scope.interval * 1000);
-      }
-
-      function getVideoData(video) {
-        var hiddenCanvas = angular.element('<canvas></canvas>')[0];
-        hiddenCanvas.width = video.width;
-        hiddenCanvas.height = video.height;
-        var ctx = hiddenCanvas.getContext('2d');
-        ctx.drawImage(video, 0, 0, video.width, video.height);
-
-        return hiddenCanvas.toDataURL();
-      }
-    }
+    controller: 'AutoSelfyCtrl'
   };
 });
